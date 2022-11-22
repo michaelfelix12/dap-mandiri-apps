f28dc816cfbedf1bee3fcb024aae2a6ef76365cf
Michael <m.felixatmadja@gmail.com>
2022-11-22 11:09:33 +0700

    [ADD] Event binding

--- src/app/demo/component-a/component-a.component.ts
+++ src/app/demo/component-a/component-a.component.ts
@@ -43,9 +43,37 @@ export class ComponentAComponent implements OnInit {
     }
   ]
 
+  canClickButton = false;
+
+  message: string = '';
+  //penerapan event binding
+  showMe(): void {
+    this.message = 'ah aku di klik';
+  }
+
+  messageHover: string = 'Hover aku dong';
+  mouseOut() {
+  this.messageHover = 'Lagi Dong';
+  }
+  mouseOver() {
+    this.messageHover = 'Thank Sist';
+  }
+
+  messageInput: string = '';
+  onMessageInput($event: any): void {
+    // console.log('$event: ', $event);
+    // console.log('$event.target.value: ', $event.target.value);
+    const { value } = $event.target;
+    if (value === 'mandiri') {
+      this.messageInput = 'aku mandirian';
+    } else {
+      this.messageInput = 'aku tetap mandirian';
+    }
+  }
+
   constructor(
     // private readonly route: ActivatedRoute
-  ) { }
+  ) {  }
 
   ngOnInit(): void {
     //   this.route.queryParams.subscribe((params) => {
@@ -53,6 +81,9 @@ export class ComponentAComponent implements OnInit {
     //   this.name = name;
     //   this.address = address;
     // })
-  }
 
+    setTimeout(() => {
+      this.canClickButton = true;
+    }, 5000);
+  }
 }
